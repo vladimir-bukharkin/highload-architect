56ccf4211629741a821919be48f8a5f3194fcda8
vbukharkin <bukharkinv@gmail.com>
2023-04-15 19:18:22 +0300

    Adds postman collection

--- src/main/java/com/otus/hw/authorization/controller/CommonController.java
+++ src/main/java/com/otus/hw/authorization/controller/CommonController.java
@@ -3,8 +3,10 @@ package com.otus.hw.authorization.controller;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -14,5 +16,11 @@ public class CommonController {
     @GetMapping(path = "/")
     public String registerUserAccount() {
         return "Авторизация прошла успешно";
+    }
+
+    @PostMapping(path = "/df")
+    public String df(@RequestBody String str) {
+        log.info("Пришло сообщение: [{}]", str);
+        return "ok";
     }
 }
